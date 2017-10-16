899bb3949f655ccb01366fa5f76967b9560ea52d
George S. Baugh <george@troglodyne.net>
2017-10-16 22:30:44 +0000

    Fix testsuite failures related to the new indexers

--- lib/App/Prove/Elasticsearch/Indexer/DzilDist.pm
+++ lib/App/Prove/Elasticsearch/Indexer/DzilDist.pm
@@ -11,16 +11,26 @@ use parent qw{App::Prove::Elasticsearch::Indexer};
 #Basically, do this:
 #our $index = `awk '/^name/ {print \$NF}' dist.ini`;
 
-our $index;
+our $index = __CLASS__->SUPER::index;
 our $dfile //= 'dist.ini';
 
-open(my $dh, '<', $dfile) or die "Could not open $dfile";
-while (<$dh>) {
-    ($index) = $_ =~ /^name\s*?=\s*?(.*)/;
-    if ($index) {
-        $index =~ s/^\s+//;
-        last;
+if ( open(my $dh, '<', $dfile) ) {
+    while (<$dh>) {
+        ($index) = $_ =~ /^name\s*?=\s*?(.*)/;
+        if ($index) {
+            $index =~ s/^\s+//;
+            last;
+        }
     }
+    close $dh;
+} else {
+    print "# WARNING: Could not open $dfile, falling back to index name '$index'\n";
 }
-close $dh;
 
+1;
+
+__END__
+
+=head2 GOTCHAS
+
+If dist.ini cannot be found, the index name will fall back to the default indexer's name.
--- lib/App/Prove/Elasticsearch/Indexer/MMDist.pm
+++ lib/App/Prove/Elasticsearch/Indexer/MMDist.pm
@@ -8,15 +8,26 @@ use warnings;
 
 use parent qw{App::Prove::Elasticsearch::Indexer};
 
-our $index;
+our $index = __CLASS__->SUPER::index;
 our $dfile //= 'Makefile.PL';
 
-open(my $dh, '<', $dfile) or die "Could not open $dfile";
-while (<$dh>) {
-    ($index) = $_ =~ /DISTNAME.*\s*?=>\s*?["|'](.*)["|'],?/;
-    if ($index) {
-        $index =~ s/^\s+//;
-        last;
+if (open(my $dh, '<', $dfile)) {;
+    while (<$dh>) {
+        ($index) = $_ =~ /DISTNAME.*\s*?=>\s*?["|'](.*)["|'],?/;
+        if ($index) {
+            $index =~ s/^\s+//;
+            last;
+        }
     }
+    close $dh;
+} else {
+    print "# WARNING: Could not open $dfile, falling back to index name '$index'\n";
 }
-close $dh;
+
+1;
+
+__END__
+
+=head2 GOTCHAS
+
+If dist.ini cannot be found, the index name will fall back to the default indexer's name.
