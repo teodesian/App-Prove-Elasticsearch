40482f11291a3b6552de463faa6a1c4bbeec8d79
George S. Baugh <george@troglodyne.net>
2018-02-09 16:34:09 -0600

    Add ingester mode to the testrail migrator as a stopgap

--- scripts/testrail-migrate.pl
+++ scripts/testrail-migrate.pl
@@ -2,8 +2,8 @@
 
 =head1 testrail_migrate.pl [OPTIONS] pattern1...patternN
 
-Migrates TestRail test results (and test plans, optionally) into the test results database.
-Only migrates plans with names matching the provided pattern.  If no patterns are provided, all will be indexed.
+Migrates TestRail test results into the test results database.
+Only migrates tests from plans/runs with names matching the provided pattern.  If no patterns are provided, all will be indexed.
 
 Requires you have a functioning ~/.testrailrc (see L<App::Prove::Plugin::TestRail>), and ~/.elastest.conf (see L<App::Prove::Elasticsearch>).
 
@@ -11,13 +11,13 @@ Requires you have a functioning ~/.testrailrc (see L<App::Prove::Plugin::TestRai
 
 =over 4
 
-=item B<--index-plan>: Index the plans (or runs) encountered much as done with [testplan].
-
 =item B<--project>: Index plans only from the provided project(s).  May be passed multiple times.
 
-=item B<--no-tests>: Don't index test results.  Only useful with --index-plan.
-
 =item B<--only-last>: Only index the last result for a given test, such as ones that had to be re-run to pass.
+
+=item B<--since>: Only index the plans which have been completed since the provided unix timestamp.
+
+=item B<--ingest>: Keep ingesting new data over and over every 5 minutes.  Allows you to enjoy (some of) the benefits of App::Prove::Elasticsearch without actually integrating it in your testsuite.
 
 =back
 
@@ -38,7 +38,25 @@ use POSIX qw{strftime};
 use App::Prove::Elasticsearch::Utils;
 use App::Prove::Elasticsearch::Indexer;
 
-main(@ARGV) unless caller();
+if (!caller()) {
+    my %ARGS = main(@ARGV);
+    exit 0 if delete $ARGS{ingest};
+    foreach my $k (keys(%ARGS)) { delete $ARGS{$k} unless $ARGS{$k}; }
+
+    my $extra_args = delete $ARGS{patterns};
+
+    my $projects = delete $ARGS{projects};
+    my $olast = delete $ARGS{'only-last'};
+    my @args = map { ("--$_" => $ARGS{$_}) } keys(%ARGS);
+    push(@args,'--only-last') if $olast;
+    push(@args,map { ('--project' => $_ ) } @$projects) if scalar(@$projects);
+    push(@args,('--ingest',@$extra_args));
+    @ARGV = @args;
+
+    print "Sleeping 5 minutes...\n";
+    sleep 300;
+    exec $0;
+}
 
 sub main {
     my @args = @_;
@@ -49,8 +67,15 @@ sub main {
         'index-plan' => \$options->{'index-plan'},
         'no-tests'   => \$options->{'no-tests'},
         'only-last'  => \$options->{'only-last'},
+        'since=i'    => \$options->{'since'},
+        ingest       => \$options->{ingest},
     );
     my @patterns = @args;
+
+
+    my %ret = %$options;
+    $ret{'since'}    = time(); #XXX this is imperfect, and could lead to double ingest, but --ingest shouldn't be used on a long term basis.
+    $ret{'patterns'} = \@patterns;
 
     my $trconf = TestRail::Utils::parseConfig(my_home());
     my $tr = TestRail::Utils::getHandle($trconf);
@@ -76,13 +101,16 @@ sub main {
     foreach my $project (@$projects) {
 
         my $runs  = $tr->getRuns($project->{id});
-        @$runs = grep {my $subj = $_; any { $subj->{name} =~ m/$_/ } @patterns } @$runs;
+        @$runs = grep {my $subj = $_; any { $subj->{name} =~ m/$_/ } @patterns } @$runs if @patterns;
 
         my $plans = $tr->getPlans($project->{id});
-        @$plans = grep {my $subj = $_; any { $subj->{name} =~ m/$_/ } @patterns } @$plans;
+        @$plans = grep {my $subj = $_; any { $subj->{name} =~ m/$_/ } @patterns } @$plans if @patterns;
 
-        #TODO handle plan upload
-        #TODO handle no test mode
+        #Filter by completed-on if provided
+        if ($options->{since}) {
+            @$runs  = grep { $_->{completed_on} && $_->{completed_on} >= $options->{since} } @$runs;
+            @$plans = grep { $_->{completed_on} && $_->{completed_on} >= $options->{since} } @$plans;
+        }
 
         foreach my $plan (@$plans) {
             my $planRuns = $tr->getChildRuns($tr->getPlanByID($plan->{id}));
@@ -97,6 +125,7 @@ sub main {
             }
         }
     }
+    return %ret;
 }
 
 sub index_test {
@@ -125,7 +154,6 @@ sub index_test {
         $test_mangled->{steps}    = translate_steps($tr,$result->{"custom_$tr->{step_field}"}) if $tr->{step_field} && $result->{"custom_$tr->{step_field}"};
 
         eval { &{ \&{$indexer . "::index_results"} }($test_mangled) }; #Bogus results aren't worth indexing
-        die $@ if $@;
         print "Couldn't index $test->{title}, skipping...\n" if $@;
         last if $tr->{'only-last'};
     }
