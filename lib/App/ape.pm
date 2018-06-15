f3d59304ce11e82c35504ccc86215f4df512b26f
George S. Baugh <george@troglodyne.net>
2018-06-15 15:25:26 -0500

    better help for ape

--- lib/App/ape.pm
+++ lib/App/ape.pm
@@ -3,6 +3,8 @@ package App::ape;
 use strict;
 use warnings;
 
+use Pod::Usage;
+
 use App::ape::test;
 use App::ape::plan;
 use App::ape::update;
@@ -11,7 +13,8 @@ sub new {
     my (undef,@args) = @_;
     my $command = shift @args;
 
-    die "You must pass a valid command to ape: plan, test or update" unless grep {$_ eq $command} qw{plan test update};
+    #I am being sneaky here and using bin/ape's POD
+    return pod2usage(0) unless grep {$_ eq $command} qw{plan test update};
 
     my $program = "App::ape::$command";
     my $program_perlized = "$program.pm";
